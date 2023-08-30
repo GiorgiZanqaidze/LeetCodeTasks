733d3c76daae46ca53fab5437511f8f1016d8e23
GiorgiZanqaidze <giorgi.zankaidze@redberry.ge>
2023-08-30 13:06:31 +0400

    task_1: finished

--- task_1.js
+++ task_1.js
@@ -4,5 +4,9 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-
+        return arr.map(fn)
 };
+
+console.log(map([1,2,3], function plusone(n) { return n + 1; }))
+console.log(map([1,2,3], fn = function constant() { return 42; }))
+console.log(map([1,2,3], fn = function plusI(n, i) { return n + i; }))
