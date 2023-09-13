5e1099439a5e419556524c51af7dea4da5e1fc22
GiorgiZanqaidze <giorgi.zankaidze@redberry.ge>
2023-09-13 23:06:03 +0400

    ffsfs

---
+++ palindrome2.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    let lowerCase = s.toLowerCase().split(' ').join('')
+    console.log(lowerCase)
+};
+
+
+console.log(isPalindrome('A man, a plan, a canal: Panama'))
